de7c6c61e1166b2ece86bdda9e9b63a4d5863825
Prashanthvari333 <81276071+Prashanthvari333@users.noreply.github.com>
2023-10-26 12:29:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const mongoose = require('mongoose')
+const authRoutes = require('./src/routes/Routes');
+const app = express();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+const connection= mongoose.connect("mongodb+srv://prashu2001:prashu2001@userauth.yrbyfkq.mongodb.net/?retryWrites=true&w=majority")
+connection.then(()=>console.log("conntected"))
+.catch(()=>{console.log("error in connecting mongo");})
+
+// Use the auth route
+
+app.get("/",(req,res)=> res.send({"email":"password"}));
+app.post("/info",
+(req,res)=>{
+    const {email,pass} = req.body;
+    console.log(email);
+    console.log(pass);
+    res.status(200).send("hello");
+}
+)
+app.use('/', authRoutes);
+app.listen(3003,()=>{console.log("Server is runing");})
